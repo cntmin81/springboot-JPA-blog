cd40d0720368f4e6e6d23a4b1d4493cbea4ac973
youngminshin <cntmin81@gmail.com>
2022-01-10 22:15:03 +0900

    환경세팅 완료

---
+++ src/test/java/io/github/cntmin81/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cntmin81.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
