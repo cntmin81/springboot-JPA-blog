486f83dd36617da16c1e1b557b58c24e259885ca
youngminshin <cntmin81@gmail.com>
2022-01-17 20:43:19 +0900

    change context

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,13 +1,13 @@
 let index = {
 	init: function() {
-		$("#btn-save").on("click", ()=> {
+		$("#btn-save").on("click", () => {
 			this.save();
 		});
-		$("#btn-login").on("click", ()=> {
+		$("#btn-login").on("click", () => {
 			this.login();
 		});
 	},
-	
+
 	save: function() {
 		//alert("save");
 		let data = {
@@ -18,18 +18,18 @@ let index = {
 		console.log(data);
 		$.ajax({
 			type: "POST",
-			url: "/blog/api/user",
+			url: "/api/user",
 			data: JSON.stringify(data), // http body data
 			contentType: "application/json; charset=utf-8", // body data type
 			dataType: "json" // 요청을 서버로해서 응답이 왔을때 기본적으로 모든것이 문자열. 타입이 json이라면 javascript오브젝트로 자동으로 변환해준다.
 		})
-		.done(function(resp){
-			alert("회원가입완료 ");
-			location.href="/blog";
-		})
-		.fail(function(error){
-			alert(JSON.stringify(error));
-		});
+			.done(function(resp) {
+				alert("회원가입완료 ");
+				location.href = "/";
+			})
+			.fail(function(error) {
+				alert(JSON.stringify(error));
+			});
 	},
 	login: function() {
 		//alert("save");
@@ -40,17 +40,17 @@ let index = {
 		console.log(data);
 		$.ajax({
 			type: "POST",
-			url: "/blog/api/user/login",
+			url: "/api/user/login",
 			data: JSON.stringify(data), // http body data
 			contentType: "application/json; charset=utf-8", // body data type
 			dataType: "json" // 요청을 서버로해서 응답이 왔을때 기본적으로 모든것이 문자열. 타입이 json이라면 javascript오브젝트로 자동으로 변환해준다.
 		})
-		.done(function(resp){
-			location.href="/blog";
-		})
-		.fail(function(error){
-			alert(JSON.stringify(error));
-		});
+			.done(function() {
+				location.href = "/";
+			})
+			.fail(function(error) {
+				alert(JSON.stringify(error));
+			});
 	}
 };
 
