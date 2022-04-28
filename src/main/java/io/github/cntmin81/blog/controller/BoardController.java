9a673c316ed1cb5e1dd0be95883bbd0cf97ba658
youngminshin <cntmin81@gmail.com>
2022-04-29 08:47:15 +0900

    글목록보기

--- src/main/java/io/github/cntmin81/blog/controller/BoardController.java
+++ src/main/java/io/github/cntmin81/blog/controller/BoardController.java
@@ -1,22 +1,28 @@
 package io.github.cntmin81.blog.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import io.github.cntmin81.blog.config.auth.PrincipalDetail;
+import io.github.cntmin81.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+
+	// 컴트롤러에서 세션을 어떻게 찾는지?
+	// public String index(@AuthenticationPrincipal PrincipalDetail principal) {
 	@GetMapping({ "", "/" })
-//	public String index(@AuthenticationPrincipal PrincipalDetail principal) {
-	public String index() {
-//		System.out.println("로그인 사용자 아이디 : " + principal.getUsername());
+	public String index(Model model) {
+		// System.out.println("로그인 사용자 아이디 : " + principal.getUsername());
+		model.addAttribute("boards", boardService.getAllList());
 		return "index";
 	}
-	
-	//USER권한이필요
+
+	// USER권한이필요
 	@GetMapping("/board/saveForm")
 	public String saveForm() {
 		return "board/saveForm";
--- src/main/java/io/github/cntmin81/blog/service/BoardService.java
+++ src/main/java/io/github/cntmin81/blog/service/BoardService.java
@@ -1,16 +1,14 @@
 package io.github.cntmin81.blog.service;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import io.github.cntmin81.blog.model.Board;
-import io.github.cntmin81.blog.model.RoleType;
 import io.github.cntmin81.blog.model.User;
 import io.github.cntmin81.blog.repository.BoardRepository;
-import io.github.cntmin81.blog.repository.UserRepository;
 
 //spring 컴포넌트스캔을 해서 bean에 등록해준다. IoC를 해줌
 //서비스레이어가 필요한 이유 
@@ -21,7 +19,7 @@ public class BoardService {
 
 	@Autowired
 	private BoardRepository boardRepository;
-	
+
 	@Transactional
 	public void writeContent(Board board, User user) {
 		board.setCount(0);
@@ -29,9 +27,14 @@ public class BoardService {
 		boardRepository.save(board);
 	}
 
-//	@Transactional(readOnly = true) // select 할태 트랜젝션시작, 서비스 종료시에 트랜젝션 종료 (정합성)
-//	public User login(User user) {
-//		return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
-//	}
+	public List<Board> getAllList() {
+		return boardRepository.findAll();
+	}
+
+	// @Transactional(readOnly = true) // select 할태 트랜젝션시작, 서비스 종료시에 트랜젝션 종료 (정합성)
+	// public User login(User user) {
+	// return userRepository.findByUsernameAndPassword(user.getUsername(),
+	// user.getPassword());
+	// }
 
 }
